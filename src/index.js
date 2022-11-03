e9b4324776bb0582160014313ec55df1718e174d
B-8128 <brijeshvadaliya1@gmail.com>
2022-11-03 16:14:34 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import About from './components/About';
+import Footer from './components/Footer';
+import Masthead from './components/Masthead';
+import Navbar from './components/Navbar';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Copyright from './components/Copyright';
+import './styles.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('brijesh')).render(
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+    <Navbar/>
+    <Masthead/>
+    <Portfolio/>
+    <About/>
+    <Contact/>
+    <Footer/>
+    <Copyright/>
+    </>
+)
